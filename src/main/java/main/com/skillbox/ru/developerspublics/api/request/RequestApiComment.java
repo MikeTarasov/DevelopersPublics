c958580396149dd2565c95ba7a021347d4598fb4
MikeTarasov <59611048+MikeTarasov@users.noreply.github.com>
2020-09-28 23:06:38 +0300

    unit tests PostCommentService

--- src/main/java/main/com/skillbox/ru/developerspublics/api/request/RequestApiComment.java
+++ src/main/java/main/com/skillbox/ru/developerspublics/api/request/RequestApiComment.java
@@ -3,9 +3,11 @@ package main.com.skillbox.ru.developerspublics.api.request;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class RequestApiComment {
     @JsonProperty("parent_id")
     Integer parentId;
--- src/test/java/com/skillbox/ru/developerspublics/test/UnitTestsPostCommentService.java
+++ src/test/java/com/skillbox/ru/developerspublics/test/UnitTestsPostCommentService.java
@@ -2,7 +2,18 @@ package com.skillbox.ru.developerspublics.test;
 
 import main.com.skillbox.ru.developerspublics.DevelopersPublicationsApplication;
 import main.com.skillbox.ru.developerspublics.api.request.RequestApiComment;
+import main.com.skillbox.ru.developerspublics.api.response.ApiCommentTrueResponse;
+import main.com.skillbox.ru.developerspublics.api.response.ErrorsResponse;
+import main.com.skillbox.ru.developerspublics.api.response.ResultFalseErrorsResponse;
+import main.com.skillbox.ru.developerspublics.model.entity.Post;
+import main.com.skillbox.ru.developerspublics.model.entity.PostComment;
+import main.com.skillbox.ru.developerspublics.model.entity.User;
+import main.com.skillbox.ru.developerspublics.model.enums.ModerationStatuses;
+import main.com.skillbox.ru.developerspublics.model.repository.PostsRepository;
+import main.com.skillbox.ru.developerspublics.model.repository.UsersRepository;
 import main.com.skillbox.ru.developerspublics.service.PostCommentService;
+import main.com.skillbox.ru.developerspublics.service.PostService;
+import main.com.skillbox.ru.developerspublics.service.UserService;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,26 +21,119 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = DevelopersPublicationsApplication.class)
 public class UnitTestsPostCommentService {
     private PostCommentService postCommentService;
+    private PostsRepository postsRepository;
+    private UsersRepository usersRepository;
+    private UserService userService;
+    private Post post;
+    private User user;
+    private final String commentText = "comment text to test comment";
 
     @Autowired
-    public void UnitTestsPostVoteService(PostCommentService postCommentService) {
+    public void UnitTestsPostVoteService(PostCommentService postCommentService, UserService userService,
+                                         PostsRepository postsRepository, UsersRepository usersRepository) {
         this.postCommentService = postCommentService;
+        this.userService = userService;
+        this.postsRepository = postsRepository;
+        this.usersRepository = usersRepository;
+    }
+
+    private void initPost() {
+        String title = "testTitle testTitle testTitle";
+        post = postsRepository.findByTitle(title);
+        if (post != null) postsRepository.delete(post);
+        String email = "testEmail";
+        String password = "testPassword";
+        user = usersRepository.findUserByEmail(email);
+        if (user != null) usersRepository.delete(user);
+        user = new User(email, "testName", password);
+        userService.saveUser(user);
+        userService.authUser(email, password);
+        post = new Post();
+        post.setIsActive(1);
+        post.setModerationStatus(ModerationStatuses.ACCEPTED.toString());
+        post.setUserId(user.getId());
+        post.setTime(System.currentTimeMillis());
+        post.setTitle(title);
+        post.setText("testText testText testText testText testText testText testText testText testText testText");
+        post.setViewCount(0);
+        postsRepository.save(post);
+    }
+
+    private void clearAll() {
+        postsRepository.delete(post);
+        usersRepository.delete(user);
+        userService.getApiAuthLogout();
     }
 
     @Test
     @Transactional
-    public void testPostApiCommentNoName() {
-        RequestApiComment request = new RequestApiComment(0, 0, "");
+    public void testPostApiCommentNonAuth() {
+        SecurityContextHolder.getContext().setAuthentication(null);
+        RequestApiComment request = new RequestApiComment(null, 0, commentText);
         ResponseEntity<?> response = postCommentService.postApiComment(request);
 
         Assert.assertEquals(response.getStatusCode(), HttpStatus.UNAUTHORIZED);
         Assert.assertNull(response.getBody());
+    }
+
+    @Test
+    @Transactional
+    public void testPostApiCommentBadParentId() {
+        initPost();
+        RequestApiComment request = new RequestApiComment(0, post.getId(), commentText);
+        ResponseEntity<?> response = postCommentService.postApiComment(request);
+
+        Assert.assertEquals(response.getStatusCode(), HttpStatus.BAD_REQUEST);
+        Assert.assertNull(response.getBody());
+        clearAll();
+    }
+
+    @Test
+    @Transactional
+    public void testPostApiCommentBadPostId() {
+        initPost();
+        RequestApiComment request = new RequestApiComment(null, 0, commentText);
+        ResponseEntity<?> response = postCommentService.postApiComment(request);
+
+        Assert.assertEquals(response.getStatusCode(), HttpStatus.BAD_REQUEST);
+        Assert.assertNull(response.getBody());
+        clearAll();
+    }
+
+    @Test
+    @Transactional
+    public void testPostApiCommentBadText() {
+        initPost();
+        RequestApiComment request = new RequestApiComment(null, post.getId(), "1");
+        ResponseEntity<?> response = postCommentService.postApiComment(request);
+
+        Assert.assertEquals(response.getStatusCode(), HttpStatus.OK);
+        Assert.assertEquals(response.getBody(), new ResultFalseErrorsResponse(new ErrorsResponse("error")));
+        clearAll();
+    }
+
+    @Test
+    @Transactional
+    public void testPostApiCommentOK() {
+        initPost();
+        RequestApiComment request = new RequestApiComment(null, post.getId(), commentText);
+        ResponseEntity<?> response = postCommentService.postApiComment(request);
+
+        Assert.assertEquals(response.getStatusCode(), HttpStatus.OK);
+        System.out.println("comment id = " + response.getBody());
+        Assert.assertNotEquals(response.getBody(), 0);
+        clearAll();
     }
 }
