ba62c92449317f5871ef4742665f0768c1a2a32e
MikeTarasov <59659611048+miketarasov@users.noreply.github.com>
2020-10-07 18:59:25 +0300

    heroku properties fix errors2

--- src/main/java/main/com/skillbox/ru/developerspublics/DevelopersPublicationsApplication.java
+++ src/main/java/main/com/skillbox/ru/developerspublics/DevelopersPublicationsApplication.java
@@ -1,26 +1,14 @@
 package main.com.skillbox.ru.developerspublics;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
-@EnableSwagger2
 @EnableScheduling
 public class DevelopersPublicationsApplication {
     public static void main(String[] args) {
         SpringApplication.run(DevelopersPublicationsApplication.class, args);
-    }
-
-    @Bean
-    public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("main.com.skillbox.ru.developerspublics")).build();
     }
 }
