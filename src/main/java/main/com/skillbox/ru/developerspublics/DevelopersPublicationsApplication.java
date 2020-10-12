4fcf9fecd042ab3df5d4f2d04883de6b30b79486
MikeTarasov <59659611048+miketarasov@users.noreply.github.com>
2020-10-12 04:46:01 +0300

    change timeZone to Moscow & setup app. timeZone

--- src/main/java/main/com/skillbox/ru/developerspublics/DevelopersPublicationsApplication.java
+++ src/main/java/main/com/skillbox/ru/developerspublics/DevelopersPublicationsApplication.java
@@ -1,13 +1,25 @@
 package main.com.skillbox.ru.developerspublics;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 
 @SpringBootApplication
 @EnableScheduling
 public class DevelopersPublicationsApplication {
+    @Value("${db.timezone}")
+    private String timeZone;
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone(timeZone));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(DevelopersPublicationsApplication.class, args);
     }
